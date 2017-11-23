f9886502aff813fd0587587ebc3bcccc5997b92f
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2017-11-23 07:23:47 +0700

    add header and footer

---
+++ application/views/templates/footer.php
@@ -0,0 +1,21 @@
+  <!-- Script goes here -->
+  <script type="text/javascript" src="<?php echo base_url('assets/js/jquery.min.js') ;?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap.min.js') ;?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('assets/js/app.js') ;?>"></script>
+
+  <script type="text/javascript" src="<?php echo base_url('plugins/datepicker/bootstrap-datepicker.js') ;?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('plugins/colorpicker/bootstrap-colorpicker.min.js') ;?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('plugins/datepicker/bootstrap-datepicker.min.js') ;?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('plugins/chartjs/Chart.min.js') ;?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('plugins/morris/morris.min.js') ;?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('plugins/nicescroll/jquery.nicescroll.min.js') ;?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('plugins/iCheck/icheck.min.js') ;?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('plugins/fastclick/fastclick.js') ;?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('plugins/fullcalendar/fullcalendar.min.js') ;?>"></script>
+
+  <script type="text/javascript" src="<?php echo base_url('assets/js/jszip.js') ;?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('assets/js/FileSaver.js') ;?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('assets/js/sweetalert2.min.js') ;?>"></script>
+
+</body>
+</html>
