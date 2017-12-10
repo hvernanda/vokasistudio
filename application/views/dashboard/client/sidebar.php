14a2e2124c36aa311ca9ba3d51c3a390cfff8fd2
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2017-12-10 20:22:15 +0700

    add dynamic sidebar

---
+++ application/views/dashboard/client/sidebar.php
@@ -0,0 +1,26 @@
+<li>
+  <a href="<?php echo base_url() ;?>">
+    <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+  </a>
+</li>
+<li class="treeview">
+  <a href="#">
+    <i class="fa fa-book"></i>
+    <span>Proyek</span>
+    <span class="pull-right-container">
+      <i class="fa fa-angle-down pull-right"></i>
+    </span>
+  </a>
+  <ul class="treeview-menu">
+    <li>
+      <a href="#">
+        <i class="fa fa-circle"></i> Semua Proyek
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa fa-circle"></i> Progress Proyek
+      </a>
+    </li>
+  </ul>
+</li>
---
+++ application/views/dashboard/manajer/sidebar.php
@@ -0,0 +1,73 @@
+<li>
+  <a href="<?php echo base_url() ;?>">
+    <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+  </a>
+</li>
+<li class="treeview">
+  <a href="#">
+    <i class="fa fa-book"></i>
+    <span>Proyek</span>
+    <span class="pull-right-container">
+      <i class="fa fa-angle-down pull-right"></i>
+    </span>
+  </a>
+  <ul class="treeview-menu">
+    <li>
+      <a href="#">
+        <i class="fa fa-circle"></i> Semua Proyek
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa fa-circle"></i> Tambah Proyek
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa fa-circle"></i> Penawaran Proyek
+      </a>
+    </li>
+  </ul>
+</li>
+<li class="treeview">
+  <a href="#">
+    <i class="fa fa-users"></i>
+    <span>Staff</span>
+    <span class="pull-right-container">
+      <i class="fa fa-angle-down pull-right"></i>
+    </span>
+  </a>
+  <ul class="treeview-menu">
+    <li>
+      <a href="#">
+        <i class="fa fa-circle"></i> Semua Staff
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa fa-circle"></i> Skill & Tools
+      </a>
+    </li>
+  </ul>
+</li>
+<li class="treeview">
+  <a href="#">
+    <i class="fa fa-user"></i>
+    <span>Client</span>
+    <span class="pull-right-container">
+      <i class="fa fa-angle-down pull-right"></i>
+    </span>
+  </a>
+  <ul class="treeview-menu">
+    <li>
+      <a href="#">
+        <i class="fa fa-circle"></i> Semua Client
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa fa-circle"></i> Semua Kontak
+      </a>
+    </li>
+  </ul>
+</li>
