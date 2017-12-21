88000161cd4c5945ec700bd04089411e009b6aab
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2017-12-21 13:48:44 +0700

    Update add_proyek.php

--- application/views/dashboard/manajer/add_proyek.php
+++ application/views/dashboard/manajer/add_proyek.php
@@ -5,11 +5,11 @@
             <label> Project Name</label>
             <input class="form-control" name="nama" id="nama" value="<?php echo set_value('nama');?>" required >
         </div>
-            <div class="form-group">
+        <div class="form-group">
             <label>Dealtime</label>
             <input type="date" class="form-control" name="Deadltime" value="<?php echo set_value('dealtime');?>" placeholder="dd-mm-yyy" required>
         </div>
-            <div class="form-group">
+        <div class="form-group">
             <label> Price</label>
         </div>
         <div class="form-group input-group">
@@ -22,11 +22,11 @@
             <label>Deadline</label>
             <input type="date" class="form-control" name="Deadline" value="<?php echo set_value('Deadline');?>" placeholder="dd-mm-yyy" required>
         </div>
-            <div class="form-group">
+        <div class="form-group">
             <label>Revision Date</label>
             <input type="date" class="form-control" name="RevisionDate" value="<?php echo set_value('RevisionDate');?>" placeholder="dd-mm-yyy" required>
         </div>
-            <div class="form-group">
+        <div class="form-group">
             <label> Down Payment</label>
         </div>
         <div class="form-group input-group">
@@ -37,4 +37,3 @@
         
         <input type="submit" class="btn btn-primary" name="submit" id="submit" value="Simpan" onClick="return confirm('Apakah anda yakin data yang anda isikan sudah benar dan sesuai?\nData yang disimpan sudah tidak dapat diubah lagi.')">
     </form>
-</div>
