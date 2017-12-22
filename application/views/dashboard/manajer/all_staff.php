f2ae7065a9905d5cfd4a6eb9cfd7f6442c1113e2
zorahima <zorahima10@gmail.com>
2017-12-22 09:11:08 +0700

    View Tampil all staff success

--- application/views/dashboard/manajer/all_staff.php
+++ application/views/dashboard/manajer/all_staff.php
@@ -1,2 +1,53 @@
 <h1> SEMUA DATA STAFF </h1>
-<?php echo var_dump($result) ;?>
+
+<div class="col-lg-12">
+<div class="panel panel-default">
+<div class="panel-heading">
+</div>
+<!-- /.panel-heading -->
+<div class="panel-body">
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Nama</th>
+                    <th> Alamat </th>
+                    <th>No.HP </th>
+                    <th> Status </th>
+                </tr>
+            </thead>
+            <tbody>
+            	  <?php
+                        $i=0;
+                        foreach($result as $staff){
+                            if($staff->id_staff){  
+                                $i++;         
+                                ?>
+            	
+	
+                <tr>
+                    <td><?php echo $i ?> </td>
+                    <td><?php echo $staff->name ?> </td>
+                    <td><?php echo $staff->address ?> </td>
+                    <td><?php echo $staff->phone ?> </td>
+                    <td><?php echo $staff->status_account ?> </td>
+                </tr>
+               
+                   <?php 
+                            }
+                        }
+
+                        ?>
+            </tbody>
+        </table>
+    </div>
+    <!-- /.table-responsive -->
+</div>
+<!-- /.panel-body -->
+</div>
+<!-- /.panel -->
+</div>
+<!-- /.col-lg-6 -->
+</div>
+<!-- /.row -->
